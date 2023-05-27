f4241999e1d618feb271b9022e5305c9ae80fca1
TingSyuanWang <vincent@fishboneapps.com>
2023-05-26 23:09:17 -0400

    feat: add eslint, prettier and husky
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- src/generateSchemaInstance.ts
+++ src/generateSchemaInstance.ts
@@ -1,75 +1,82 @@
 export function generateSchemaInstance(schema: any, data: any): any {
-    let profile: any = {}
+  let profile: any = {}
 
-    Object.keys(data)
-        .filter(fieldName => data[fieldName] !== '')
-        .forEach(fieldName => {
-            if (fieldName === 'linked_schemas') {
-                profile[fieldName] = data[fieldName].split(',').map((s: string) => s.trim())
-            } else if (fieldName.includes('[') || fieldName.includes('.')) {
-                profile = parseArrayObject(fieldName, data[fieldName], schema, profile)
-            } else {
-                if (schema?.properties[fieldName]?.type === 'number') {
-                    profile[fieldName] = parseFloat(data[fieldName])
-                } else {
-                    profile[fieldName] = data[fieldName]
-                }
-            }
-        })
-    return profile
+  Object.keys(data)
+    .filter(fieldName => data[fieldName] !== '')
+    .forEach(fieldName => {
+      if (fieldName === 'linked_schemas') {
+        profile[fieldName] = data[fieldName]
+          .split(',')
+          .map((s: string) => s.trim())
+      } else if (fieldName.includes('[') || fieldName.includes('.')) {
+        profile = parseArrayObject(fieldName, data[fieldName], schema, profile)
+      } else {
+        if (schema?.properties[fieldName]?.type === 'number') {
+          profile[fieldName] = parseFloat(data[fieldName])
+        } else {
+          profile[fieldName] = data[fieldName]
+        }
+      }
+    })
+  return profile
 }
 
-function parseArrayObject(fieldName: string, fieldData: any, schema: any, profile: any): any {
-    let props = fieldName.split('.')
-    let curr = profile
-    let currSchema = schema
+function parseArrayObject(
+  fieldName: string,
+  fieldData: any,
+  schema: any,
+  profile: any
+): any {
+  const props = fieldName.split('.')
+  let curr = profile
+  let currSchema = schema
 
-    for (let i = 0; i < props.length; i++) {
-        let prop = props[i]
-        let matches = prop.match(/(.+)\[(\d+)]/)
+  for (let i = 0; i < props.length; i++) {
+    const prop = props[i]
+    const matches = prop.match(/(.+)\[(\d+)]/)
 
-        if (matches) {
-            let name = matches[1]
-            let index = parseInt(matches[2])
+    if (matches) {
+      const name = matches[1]
+      const index = parseInt(matches[2])
 
-            // Create the array if it doesn't exist yet
-            if (!curr[name]) {
-                curr[name] = []
-            }
+      // Create the array if it doesn't exist yet
+      if (!curr[name]) {
+        curr[name] = []
+      }
 
-            // Create the object at the specified index if it doesn't exist yet
-            if (props.length > 1 && !curr[name][index]) {
-                curr[name][index] = {}
-            }
+      // Create the object at the specified index if it doesn't exist yet
+      if (props.length > 1 && !curr[name][index]) {
+        curr[name][index] = {}
+      }
 
-            // Move the current pointer to the object at the specified index
-            if (i === props.length - 1) {
-                if (currSchema?.properties[name]?.items?.type === 'number') {
-                    fieldData = parseFloat(fieldData)
-                }
-                curr[name][index] = fieldData
-            } else {
-                curr = curr[name][index]
-                currSchema = currSchema?.properties[name]?.items
-            }
-        } else {
-            // Create the object if it doesn't exist yet
-            if (!curr[prop]) {
-                curr[prop] = {}
-            }
-
-            // Move the current pointer to the object
-            if (i === props.length - 1) {
-                if (currSchema?.properties[prop]?.type === 'number') {
-                    fieldData = parseFloat(fieldData)
-                }
-                curr[prop] = fieldData
-            } else {
-                curr = curr[prop]
-                currSchema = currSchema?.properties[prop]
-            }
+      // Move the current pointer to the object at the specified index
+      if (i === props.length - 1) {
+        if (currSchema?.properties[name]?.items?.type === 'number') {
+          fieldData = parseFloat(fieldData)
         }
-    }
+        curr[name][index] = fieldData
+      } else {
+        curr = curr[name][index]
+        currSchema = currSchema?.properties[name]?.items
+      }
+    } else {
+      // Create the object if it doesn't exist yet
+      if (!curr[prop]) {
+        curr[prop] = {}
+      }
 
-    return profile
+      // Move the current pointer to the object
+      if (i === props.length - 1) {
+        if (currSchema?.properties[prop]?.type === 'number') {
+          fieldData = parseFloat(fieldData)
+        }
+        curr[prop] = fieldData
+      } else {
+        curr = curr[prop]
+        currSchema = currSchema?.properties[prop]
+      }
+    }
+  }
+
+  return profile
 }
--- tests/generateSchemaInstance.test.js
+++ tests/generateSchemaInstance.test.js
@@ -12,29 +12,27 @@ describe('generateSchemaInstance', () => {
           street: { type: 'string' },
           city: { type: 'string' },
           state: { type: 'string' },
-          zip: { type: 'string' },
-        },
+          zip: { type: 'string' }
+        }
       },
       phone_numbers: {
         type: 'array',
         items: {
-          type: 'string',
-        },
-      },
-    },
+          type: 'string'
+        }
+      }
+    }
   }
 
   it('should generate a profile with all fields', () => {
     const data = {
       name: 'John Doe',
       age: '30',
-      address: {
-        street: '123 Main St',
-        city: 'Anytown',
-        state: 'CA',
-        zip: '12345',
-      },
-      phone_numbers: ['555-1234', '555-5678'],
+      'address.street': '123 Main St',
+      'address.city': 'Anytown',
+      'address.state': 'CA',
+      'address.zip': '12345',
+      phone_numbers: ['555-1234', '555-5678']
     }
 
     const expectedProfile = {
@@ -44,9 +42,9 @@ describe('generateSchemaInstance', () => {
         street: '123 Main St',
         city: 'Anytown',
         state: 'CA',
-        zip: '12345',
+        zip: '12345'
       },
-      phone_numbers: ['555-1234', '555-5678'],
+      phone_numbers: ['555-1234', '555-5678']
     }
 
     const profile = generateSchemaInstance(schema, data)
@@ -58,43 +56,20 @@ describe('generateSchemaInstance', () => {
     const data = {
       name: 'Jane Smith',
       age: '',
-      address: {
-        street: '',
-        city: 'Anytown',
-        state: 'CA',
-        zip: '',
-      },
-      phone_numbers: [],
+      'address.street': '',
+      'address.city': 'Anytown',
+      'address.state': 'CA',
+      'address.zip': '',
+      phone_numbers: []
     }
 
     const expectedProfile = {
       name: 'Jane Smith',
       address: {
         city: 'Anytown',
-        state: 'CA',
+        state: 'CA'
       },
-      phone_numbers: [],
-    }
-
-    const profile = generateSchemaInstance(schema, data)
-    console.log(profile)
-
-    expect(profile).toEqual(expectedProfile)
-  })
-
-  it('should generate a profile with array objects', () => {
-    const data = {
-      'address.street': ['123 Main St', '456 Second St'],
-      'address.city': ['Anytown', 'Othertown'],
-      'address.state': ['CA', 'NY'],
-      'address.zip': ['12345', '67890'],
-    }
-
-    const expectedProfile = {
-      address: [
-        { street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-        { street: '456 Second St', city: 'Othertown', state: 'NY', zip: '67890' },
-      ],
+      phone_numbers: []
     }
 
     const profile = generateSchemaInstance(schema, data)
@@ -104,11 +79,11 @@ describe('generateSchemaInstance', () => {
 
   it('should generate a profile with linked schemas', () => {
     const data = {
-      linked_schemas: 'schema1, schema2, schema3',
+      linked_schemas: 'schema1, schema2, schema3'
     }
 
     const expectedProfile = {
-      linked_schemas: ['schema1', 'schema2', 'schema3'],
+      linked_schemas: ['schema1', 'schema2', 'schema3']
     }
 
     const profile = generateSchemaInstance(schema, data)
